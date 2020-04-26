784d8a001c051623e0ca05e99b9c2351ba0a9d2b
ktajima <>
2020-04-26 13:58:34 +0900

    routes修正

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'coderay'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', groups: %w(test development), require: false
-gem 'pg', groups: %w(production), require: false
+# gem 'pg', groups: %w(production), require: false
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
+
+  # 各カテゴリの一覧取得
   get  '/js',         to: 'static_pages#js'
   get  '/jquery',     to: 'static_pages#jquery'
   get  '/rails',      to: 'static_pages#rails'
-
-  get  '/help',   to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
+  # 静的ページ参照
+  get  '/help',      to: 'static_pages#help'
+  get  '/about',     to: 'static_pages#about'
   get  '/sitemap',   to: 'static_pages#sitemap'
-  get  '/contact', to: 'static_pages#contact'
-  # get  '/signup',  to: 'users#new'
-
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get  '/contact',   to: 'static_pages#contact'
+  # 管理者登録/ログイン
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :account_activations, only: [:edit]
   resources :users
   resources :microposts
-  resources :mydashboard,          only: [:create, :destroy, :show]
+  resources :mydashboard, only: [:create, :destroy, :show]
 end
