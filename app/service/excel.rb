6f0df4e8a39b1a6bc767389ebd4cbbd056c72d4f
tttkkkkk <test_t_55@yahoo.co.jp>
2020-06-14 20:25:32 +0900

    簡単なファイル出力できた

--- app/service/excel.rb
+++ app/service/excel.rb
@@ -1,19 +1,28 @@
-module Excel
 
+module Excel
+  require 'csv'
+  require 'roo'
+
+  CSV_EXPORT_HEADER = %w(処理区分 ブランド 店舗名)
   #[実行の手順]
   #0 gem install roo  （インストール済ならスキップ）
   #1 ターミナル起動
   #2 cd app/service
-  #3 ruby excel.rb 
-  require 'roo'
-
+  #3 エクセル名の確認
+  #4 ruby excel.rb
   xlsx = Roo::Excelx.new('sample.xlsx')
   sheet = xlsx.sheet(0)
-  # p sheet.last_row      #行
-  # p sheet.last_column   #列
-
-  # データ出力
-  2.upto(xlsx.last_row) do |row|  
-    puts "#{xlsx.cell(row, 9)} #{xlsx.cell(row, 10)}"
+  #ヘッダー
+  headers = CSV_EXPORT_HEADER
+  #データ
+  outputs = []
+  3.upto(xlsx.last_row) do |row|
+    outputs << "a,#{xlsx.cell(row, 9)},#{xlsx.cell(row, 10)}"
   end
+
+  #CSVファイル出力
+  f = File.open("#{Time.now.strftime('%Y%m%d-%H%M')}.csv", 'w') # wは書き込み権限
+  f.puts headers.join(',')
+  f.puts outputs
+  f.close
 end
